0414bee2e146e232757b07d1857be9c1d3011a38
Vivena <edward.guyot@gmail.com>
2023-06-27 23:10:26 +0800

    Add multi-threading, need refacto

---
+++ kms/source/keyManagmentAgent/App.cpp
@@ -0,0 +1,30 @@
+#include "keyManagmentAgent/"
+
+void run() {
+
+  /* Register Components in scope of run() method */
+  example::book::AppComponent components(
+    {"localhost", 8002},      // Book Service
+    {"book.virtualhost", 0}   // Book Service Virtual Host
+  );
+
+  /* run */
+  std::list<std::thread> acceptingThreads;
+
+  example::book::Runner runner;
+  runner.run(acceptingThreads);
+
+  for(auto& thread : acceptingThreads) {
+    thread.join();
+  }
+
+}
+
+int main(int argc, const char * argv[]) {
+
+  oatpp::base::Environment::init();
+  run();
+  oatpp::base::Environment::destroy();
+  
+  return 0;
+}
